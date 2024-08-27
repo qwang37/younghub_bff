a085933a5693a7c9a5dc60ca756ecb1a3a908304
Bill Wang <wangbi@microsoft.com>
2024-08-27 16:09:39 -0500

    fix the valid path

--- app/videos/routes.py
+++ app/videos/routes.py
@@ -8,7 +8,7 @@ def list_videos():
     video_url = "https://younghubstorage.blob.core.windows.net/short-video"
     videos = [
         {"ID": 1, "VideoURL": video_url + "/introduction_1.mp4"},
-        {"ID": 2, "VideoURL": video_url + "introduction_2.mp4"},
+        {"ID": 2, "VideoURL": video_url + "/introduction_2.mp4"},
         {"ID": 3, "VideoURL": video_url + "/introduction_3.mp4"}
     ]
     return jsonify(videos)

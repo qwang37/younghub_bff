5342d711a97b2e84bc066f26bd79a14b220f9e34
Bill Wang <wangbi@microsoft.com>
2024-08-27 16:34:20 -0500

    change index from zero

--- app/videos/routes.py
+++ app/videos/routes.py
@@ -7,8 +7,8 @@ video_bp = Blueprint('videos', __name__)
 def list_videos():
     video_url = "https://younghubstorage.blob.core.windows.net/short-video"
     videos = [
-        {"ID": 1, "VideoURL": video_url + "/introduction_1.mp4"},
-        {"ID": 2, "VideoURL": video_url + "/introduction_2.mp4"},
-        {"ID": 3, "VideoURL": video_url + "/introduction_3.mp4"}
+        {"ID": 0, "VideoURL": video_url + "/introduction_1.mp4"},
+        {"ID": 1, "VideoURL": video_url + "/introduction_2.mp4"},
+        {"ID": 2, "VideoURL": video_url + "/introduction_3.mp4"}
     ]
     return jsonify(videos)
